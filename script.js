73a2d1229fcf8962dd1d408b45d2ac84df47fbf9
bluegamer3683Git <46286090+bluegamer3683Git@users.noreply.github.com>
2023-06-12 01:43:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,30 @@
 // @namespace   YourNamespace
 // @version     1.0
 // @description Removes ads on Imgur
-// @match       https://imgur.com/*
+// @match       https://imgur.io/*
 // @grant       none
 // ==/UserScript==
+
 
 (function() {
     'use strict';
 
-    // Remove ads
-    var ads = document.querySelectorAll('.ad-container');
-    ads.forEach(function(ad) {
-        ad.style.display = 'none';
+    function removeAds() {
+        var ads = document.querySelectorAll('[id*="noad"]');
+        ads.forEach(function(ad) {
+            ad.style.display = 'none';
+        });
+
+      //console.log("removing ads!");
+    }
+
+    // Remove initial ads
+    removeAds();
+
+    // Remove ads as you scroll
+    window.addEventListener('scroll', function() {
+       removeAds();
     });
 
-    // Enjoy ad-free Imgur!
+    // Enjoy an uninterrupted scrolling experience on Imgur!
 })();
